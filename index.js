f074b04e96a669076b89679d2f34a52285fdc711
jorge <eduardcrow555@gmail.com>
2022-05-07 10:32:22 -0300

    estudandos as rotas

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get("/",  (req, res) =>{
+  res.send('Hello');
+});
+
+
+app.get("/home", (req , res )=>{
+res.send("olá " )
 })
 
-app.listen(3000)
+app.listen(3000);
+
+
