2596067c55ae996273270042a64d545e434738f1
jorge <eduardcrow555@gmail.com>
2022-05-07 20:42:54 -0300

    cards

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-const express = require('express'); //inporta o express
+app.set("view engine", "ejs");
+const express = require("express");
 const app = express();
 
-app.get("/",  (req, res) =>{
-  res.send('Hello');
+
+const guitars = [
+    {
+      id : "JETBLUES COSMOS",
+      madeiras: "cedro , pau marfin , pau, marupa ",
+      capitadores: " 2 Tagima Silverbucker Alnico 5 com cover de metal",
+      Imagen:"http://www.tagima.com.br/uploads/produtos/jetblues_cosmos_g1.jpg",
+    }
+
+]
+
+
+
+
+
+
+
+
+app.set('view engine', 'ejs');
+
+
+app.get("/", ( req, res )=>{
+res.render("index", guitars);
 });
 
+app.listen(3000,( ) =>{
 
-app.get("/", (req , res )=>{
-res.render("views");
 })
-
-app.listen(3000);
-
-
