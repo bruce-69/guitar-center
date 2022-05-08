0568d3342fc2242f7d7949a83fafa867fe86459f
jorge <eduardcrow555@gmail.com>
2022-05-08 10:31:39 -0300

    cards iniciais

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-app.set("view engine", "ejs");
 const express = require("express");
 const app = express();
 
-
 const guitars = [
-    {
-      id : "JETBLUES COSMOS",
-      madeiras: "cedro , pau marfin , pau, marupa ",
-      capitadores: " 2 Tagima Silverbucker Alnico 5 com cover de metal",
-      Imagen:"http://www.tagima.com.br/uploads/produtos/jetblues_cosmos_g1.jpg",
-    }
+   
+      {
+        id: "TRUE-RANGE 7",
+         
+        madeiras: "cedro .pau-marfin",
 
-]
+        captadores: "2-BDB-Slant-alnico-humbuckers-by-Zaganin-Pickups",
+         
+        imagem: "http://www.tagima.com.br/uploads/produtos/truerange7_g1.jpg",
 
+      },
 
-
-
-
-
-
-
-app.set('view engine', 'ejs');
+         ] 
 
 
-app.get("/", ( req, res )=>{
-res.render("index", guitars);
+
+
+app.set("view engine", "ejs")
+
+app.get("/", function (req, res) {
+  res.render('index',{guitars});
 });
 
-app.listen(3000,( ) =>{
 
-})
+
+app.listen(3000);
